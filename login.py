40fe4ad6eaf85dbb056adb4538fc571dbfdea580
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2020-06-30 21:41:31 -0700

    added procfile

--- login.py
+++ login.py
@@ -2,13 +2,10 @@ import os
 
 def login():
     print ("Logging in..")
-    try:
-        r = praw.Reddit(username = os.environ["reddit_username"],
+    r = praw.Reddit(username = os.environ["reddit_username"],
                 password = os.environ["reddit_password"],
                 client_id = os.environ["client_id"],
                 client_secret = os.environ["client_secret"],
                 user_agent = "Reddit C_Prest Bot 0.1")
-        print ("Logged in!")
-    except:
-        print ("Failed to log in!")
+    print ("Logged in!")
     return r

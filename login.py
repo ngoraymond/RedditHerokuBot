a4896bc0aef1a690e98f0d0827bc696ab19c41af
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2020-07-21 20:09:14 -0700

    DONE. NO MORE CHANGES

--- login.py
+++ login.py
@@ -8,7 +8,7 @@ def login():
                 password = os.environ["reddit_password"],
                 client_id = os.environ["client_id"],
                 client_secret = os.environ["client_secret"],
-                user_agent = "Reddit C_Prest Repo Bot 1.17")
+                user_agent = "Reddit C_Prest Re 1.189")
         print ("Logged in!")
     except:
         print ("Failed to log in!")
@@ -21,7 +21,7 @@ def commentBotLogin():
                 password = os.environ["reddit_password"],
                 client_id = os.environ["client_id_2"],
                 client_secret = os.environ["client_secret_2"],
-                user_agent = "Reddit TopCom Bot1 1.17")
+                user_agent = "topCOMBO 1.017")
         print ("Logged in!")
     except:
         print ("Failed to log in!")

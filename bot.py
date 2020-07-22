e40fcbeb1478643ee9e7691e9dabb75d70ee00e2
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2020-07-21 18:18:21 -0700

    adding comments

--- bot.py
+++ bot.py
@@ -8,15 +8,15 @@ import login
 
 
 def main():
-    reddit = login.login()
+    poster = login.login()
     commentBot = login.commentBotLogin()
-    subred = reddit.subreddit('College_Prestige')
+    subred = poster.subreddit('College_Prestige')
     risingSubs = ['all','funny','memes','dankmemes','wallstreetbets','stocks']
     index = 0
     while True:
         print("--------------------------------------")
         # Get top 5 rising posts in the risingSubs list
-        currentSub = reddit.subreddit(risingSubs[index])
+        currentSub = poster.subreddit(risingSubs[index])
         print('r/'+risingSubs[index])
         for submission in currentSub.rising(limit=5):
             print(submission.title)
@@ -41,15 +41,16 @@ def main():
                     f.write('\n')
             time.sleep(random.randint(5,30))
             #Create permalink of original post as a comment
-            for subReply in reddit.redditor(os.environ['reddit_username']).submissions.new(limit=1):
-                commentBot.submission(id=subReply.id).reply('From: https://www.reddit.com'+submission.permalink)
+            for subReply in poster.redditor(os.environ['reddit_username']).submissions.new(limit=1):
+                poster.submission(id=subReply.id).reply('From: https://www.reddit.com'+submission.permalink)
                 time.sleep(random.randint(3,10))
                 #Upvote the repost from my other bot
                 #commentBot.submission(id=subReply.id).upvote()
                 time.sleep(random.randint(3,10))
-                for comment in submission.comments:
-                    commentBot.submission(id=subReply.id).reply(comment.body)
-                    time.sleep(random.randint(3,10))
+                if submission.permalink[3:7] != 'rpan':
+                    for comment in submission.comments:
+                        commentBot.submission(id=subReply.id).reply(comment.body)
+                        time.sleep(random.randint(3,10))
         #move on to the next subreddit
         index = (index+1)%(len(risingSubs))
         #wait up to 10 minutes
